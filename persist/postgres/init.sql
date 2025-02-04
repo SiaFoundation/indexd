f34ed29c5f4d01ebcfdaf19aec85e047522f079e
Nate <me@n8m.us>
2025-02-04 06:53:12 -0800

    consistent index names

--- persist/postgres/init.sql
+++ persist/postgres/init.sql
@@ -8,8 +8,8 @@ CREATE TABLE syncer_bans (
     expiration TIMESTAMP WITH TIME ZONE NOT NULL,
     reason TEXT NOT NULL
 );
-CREATE INDEX syncer_bans_expiration_index_idx ON syncer_bans (expiration);
-CREATE INDEX syncer_bans_net_cidr_gist_idx ON syncer_bans USING gist (net_cidr inet_ops); -- fast subnet matches
+CREATE INDEX syncer_bans_expiration_idx ON syncer_bans (expiration);
+CREATE INDEX syncer_bans_net_cidr_idx ON syncer_bans USING gist (net_cidr inet_ops); -- fast subnet matches
 
 CREATE TABLE global_settings (
     id INTEGER PRIMARY KEY NOT NULL DEFAULT 0 CHECK (id = 0), -- enforce a single row
