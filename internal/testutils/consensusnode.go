739a2f9af9437e8a2cee28ddf7617a17f6d3ef8c
Chris Schinnerl <chris@sia.tech>
2025-02-24 13:46:07 +0100

    add consensusnode.go

---
+++ internal/testutils/consensusnode.go
@@ -0,0 +1,68 @@
+package testutils
+
+import (
+	"testing"
+	"time"
+
+	"go.sia.tech/core/consensus"
+	"go.sia.tech/core/types"
+	"go.sia.tech/coreutils"
+	"go.sia.tech/coreutils/chain"
+	"go.sia.tech/coreutils/testutil"
+	"go.uber.org/zap"
+)
+
+// ConsensusNode is a helper type that serves as the source of truth for
+// anything consensus-related during testing. It can be used to create test
+// types such as the Indexer or Wallet and will guarantee that a call to
+// `MineBlock` won't return before all created components are done subscribing
+// to the newly mined blocks.
+type ConsensusNode struct {
+	tb      testing.TB
+	cm      *chain.Manager
+	genesis types.Block
+	network *consensus.Network
+
+	syncFns []func()
+}
+
+// NewConsensusNode creates a new node ready for testing. It will mine enough
+// blocks to reach the V2 require height before returning.
+func NewConsensusNode(tb testing.TB, log *zap.Logger) *ConsensusNode {
+	network, genesis := testutil.V2Network()
+	dbstore, tipState, err := chain.NewDBStore(chain.NewMemDB(), network, genesis)
+	if err != nil {
+		tb.Fatalf("failed to create chain store: %v", err)
+	}
+	cm := chain.NewManager(dbstore, tipState, chain.WithLog(log.Named("chain")))
+	c := &ConsensusNode{
+		cm:      cm,
+		tb:      tb,
+		genesis: genesis,
+		network: network,
+	}
+	c.MineBlocks(types.VoidAddress, network.HardforkV2.RequireHeight)
+	return c
+}
+
+// MineBlocks is a helper to mine blocks and broadcast the headers
+func (c *ConsensusNode) MineBlocks(addr types.Address, n uint64) {
+	c.tb.Helper()
+
+	for i := uint64(0); i < n; i++ {
+		b, ok := coreutils.MineBlock(c.cm, addr, 5*time.Second)
+		if !ok {
+			c.tb.Fatal("failed to mine block")
+		} else if err := c.cm.AddBlocks([]types.Block{b}); err != nil {
+			c.tb.Fatal(err)
+		}
+	}
+	for _, fn := range c.syncFns {
+		fn()
+	}
+}
+
+// Network returns the used network configuration of the ConsensusNode
+func (c *ConsensusNode) Network() *consensus.Network {
+	return c.network
+}
