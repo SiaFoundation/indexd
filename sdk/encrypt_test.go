84fa65c2d7d72a1463e9751a73d9ad5757a428e5
Christopher Tarry <chrisjtarry@gmail.com>
2025-08-20 11:57:12 -0400

    test beyond the counter limit

--- sdk/encrypt_test.go
+++ sdk/encrypt_test.go
@@ -17,8 +17,34 @@ func TestEncryptRoundtrip(t *testing.T) {
 	frand.Read(key[:])
 
 	for _, offset := range []uint64{0, 64, 128, 2048, 4096} {
-		t.Run(fmt.Sprintf("offset %d", offset), func(t *testing.T) {
+		t.Run(fmt.Sprint(offset), func(t *testing.T) {
 			expected := data[offset:]
+			r, err := encrypt(&key, bytes.NewReader(expected), offset)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			read, err := io.ReadAll(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			var buf bytes.Buffer
+			decrypted := decrypt(&key, &buf, offset)
+			if _, err := decrypted.Write(read); err != nil {
+				t.Fatal(err)
+			}
+
+			if !bytes.Equal(expected, buf.Bytes()) {
+				t.Fatalf("data mismatch: expected %v, got %v", expected, buf.Bytes())
+			}
+		})
+	}
+
+	for _, offset := range []uint64{maxBytesPerNonce, 2 * maxBytesPerNonce} {
+		t.Run(fmt.Sprint(offset), func(t *testing.T) {
+			// test offset beyond max bytes
+			expected := data[:]
 			r, err := encrypt(&key, bytes.NewReader(expected), offset)
 			if err != nil {
 				t.Fatal(err)
