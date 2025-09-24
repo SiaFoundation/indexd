a055a34d7553d9caab70a5014a124b23c6aecdb5
PJ <peterjan.brone@gmail.com>
2025-09-24 14:33:16 +0200

    all: change fields

--- api/admin/admin_test.go
+++ api/admin/admin_test.go
@@ -796,8 +796,8 @@ func TestSectorStatsAPI(t *testing.T) {
 	stats, err := adminClient.StatsSectors(context.Background())
 	if err != nil {
 		t.Fatal(err)
-	} else if stats.NumSlabs != 0 {
-		t.Fatalf("expected no slabs, got %d", stats.NumSlabs)
+	} else if stats.Slabs != 0 {
+		t.Fatalf("expected no slabs, got %d", stats.Slabs)
 	}
 
 	// pin a slab
@@ -820,8 +820,8 @@ func TestSectorStatsAPI(t *testing.T) {
 	stats, err = adminClient.StatsSectors(context.Background())
 	if err != nil {
 		t.Fatal(err)
-	} else if stats.NumSlabs != 1 {
-		t.Fatalf("expected 1 slab, got %d", stats.NumSlabs)
+	} else if stats.Slabs != 1 {
+		t.Fatalf("expected 1 slab, got %d", stats.Slabs)
 	}
 
 	// unpin the slab
@@ -833,8 +833,8 @@ func TestSectorStatsAPI(t *testing.T) {
 	stats, err = adminClient.StatsSectors(context.Background())
 	if err != nil {
 		t.Fatal(err)
-	} else if stats.NumSlabs != 0 {
-		t.Fatalf("expected no slabs, got %d", stats.NumSlabs)
+	} else if stats.Slabs != 0 {
+		t.Fatalf("expected no slabs, got %d", stats.Slabs)
 	}
 }
 
