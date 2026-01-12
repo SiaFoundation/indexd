aa5519c283fba80bea284350cefeab0a650d6aa4
Christopher Tarry <chrisjtarry@gmail.com>
2026-01-12 11:54:49 -1000

    remove mock store from tests

--- contracts/manager_test.go
+++ contracts/manager_test.go
@@ -1,43 +1,46 @@
-package contracts
+package contracts_test
 
 import (
 	"context"
 	"errors"
-	"reflect"
 	"testing"
 
-	"go.sia.tech/core/rhp/v4"
 	"go.sia.tech/core/types"
+	"go.sia.tech/indexd/contracts"
 	"go.sia.tech/indexd/hosts"
 )
 
 func TestBlockBadHosts(t *testing.T) {
-	store := &storeMock{}
-	hmMock := &hostManagerMock{store: store, settings: make(map[types.PublicKey]rhp.HostSettings)}
-	contracts := newContractManager(types.PublicKey{}, nil, nil, nil, store, nil, hmMock, nil, nil)
+	store := newContractTestStore(t)
+	hmMock := newHostManagerMock(store)
+	cm := contracts.NewTestContractManager(types.PublicKey{}, nil, nil, nil, store, nil, hmMock, nil, nil)
 
-	goodHost := hosts.Host{PublicKey: types.PublicKey{1}, Usability: hosts.GoodUsability, Blocked: false}
-	badHost := hosts.Host{PublicKey: types.PublicKey{2}, Usability: hosts.Usability{}, Blocked: false}
-	unusedBadHost := hosts.Host{PublicKey: types.PublicKey{3}, Usability: hosts.Usability{}, Blocked: false}
+	goodHost := hosts.Host{PublicKey: types.PublicKey{1}, Usability: hosts.GoodUsability, Settings: goodSettings}
+	badHost := hosts.Host{PublicKey: types.PublicKey{2}, Usability: hosts.Usability{}, Settings: goodSettings}
+	unusedBadHost := hosts.Host{PublicKey: types.PublicKey{3}, Usability: hosts.Usability{}, Settings: goodSettings}
 
-	store.hosts = map[types.PublicKey]hosts.Host{
-		goodHost.PublicKey:      goodHost,
-		badHost.PublicKey:       badHost,
-		unusedBadHost.PublicKey: unusedBadHost,
-	}
+	// add hosts to the store
+	store.addTestHost(t, goodHost)
+	store.addTestHost(t, badHost)
+	store.addTestHost(t, unusedBadHost)
+
+	// add settings to host manager
+	hmMock.settings[goodHost.PublicKey] = goodSettings
+	hmMock.settings[badHost.PublicKey] = goodSettings
+	hmMock.settings[unusedBadHost.PublicKey] = goodSettings
 
 	// form a contract with each host except the unused one
 	for _, host := range []hosts.Host{goodHost, badHost} {
-		store.addTestContract(t, host.PublicKey, true)
+		store.addTestContract(t, host.PublicKey, true, types.FileContractID(host.PublicKey))
 	}
 
 	// block the bad hosts
-	if err := contracts.blockBadHosts(context.Background()); err != nil {
+	if err := cm.BlockBadHosts(context.Background()); err != nil {
 		t.Fatal(err)
 	}
 
 	// helper to assert a blocked host has a blocked contract and vice versa
-	assertHostAndContract := func(hk types.PublicKey, blocked bool, reasons []string) {
+	assertHostAndContract := func(hk types.PublicKey, blocked bool, expectReasons bool) {
 		t.Helper()
 
 		host, err := store.Host(hk)
@@ -45,11 +48,11 @@ func TestBlockBadHosts(t *testing.T) {
 			t.Fatal(err)
 		} else if host.Blocked != blocked {
 			t.Fatalf("expected host %v to be blocked=%v, got blocked=%v", hk, blocked, host.Blocked)
-		} else if host.Blocked && !reflect.DeepEqual(host.BlockedReasons, reasons) {
-			t.Fatalf("expected host %v to be blocked due to %v, got blocked due to %v", hk, reasons, host.BlockedReasons)
+		} else if host.Blocked && expectReasons && len(host.BlockedReasons) == 0 {
+			t.Fatalf("expected host %v to have block reasons, got none", hk)
 		}
 		contract, err := store.Contract(types.FileContractID(hk))
-		if errors.Is(err, ErrNotFound) && hk == unusedBadHost.PublicKey {
+		if errors.Is(err, contracts.ErrNotFound) && hk == unusedBadHost.PublicKey {
 			return // unused host doesn't have a contract
 		} else if err != nil {
 			t.Fatal(err)
@@ -59,11 +62,11 @@ func TestBlockBadHosts(t *testing.T) {
 	}
 
 	// a good host shouldn't be blocked
-	assertHostAndContract(goodHost.PublicKey, false, nil)
+	assertHostAndContract(goodHost.PublicKey, false, false)
 
 	// a bad host and its contract should be blocked
-	assertHostAndContract(badHost.PublicKey, true, badHost.Usability.FailedChecks())
+	assertHostAndContract(badHost.PublicKey, true, true)
 
 	// an unused host shouldn't be blocked
-	assertHostAndContract(unusedBadHost.PublicKey, false, nil)
+	assertHostAndContract(unusedBadHost.PublicKey, false, false)
 }
