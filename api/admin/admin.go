229651b2ebb8f3db6e4d71125c69a2d7e99eb006
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2026-03-09 15:59:07 +0100

    return 204

--- api/admin/admin.go
+++ api/admin/admin.go
@@ -594,6 +594,7 @@ func (a *admin) handleDELETEAccountPrune(jc jape.Context) {
 		jc.Error(err, http.StatusInternalServerError)
 		return
 	}
+	jc.Encode(nil)
 }
 
 func (a *admin) handlePATCHAccount(jc jape.Context) {
