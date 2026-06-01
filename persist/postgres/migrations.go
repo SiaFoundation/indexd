fa7130ad0067a5ec666d15ecfaa231429a4ee591
PJ <peterjan.brone@gmail.com>
2026-06-01 09:43:02 +0200

    store: drop unused sectors_next_integrity_check_idx

--- persist/postgres/migrations.go
+++ persist/postgres/migrations.go
@@ -86,4 +86,8 @@ UPDATE hosts SET has_quic = EXISTS (
 		_, err := tx.Exec(ctx, `ALTER TABLE hosts DROP COLUMN has_bad_quic_port`)
 		return err
 	},
+	func(ctx context.Context, tx *txn, log *zap.Logger) error {
+		_, err := tx.Exec(ctx, `DROP INDEX IF EXISTS sectors_next_integrity_check_idx`)
+		return err
+	},
 }
